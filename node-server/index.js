43939a05ca901dcada41c48dc0c2cd09fda9768a
krunoo53k <89876822+krunoo53k@users.noreply.github.com>
2022-06-07 09:44:18 +0200

    Enabling CORS for all origins

--- node-server/index.js
+++ node-server/index.js
@@ -12,6 +12,12 @@ var con = mysql.createConnection({
   database        : process.env.DB_DATABASE 
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/kviz/:id',(req,res) =>{
     const {id} = req.params;
     if(isNaN(id))
