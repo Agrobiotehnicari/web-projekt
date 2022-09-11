b68f68faf47956fdf369470e06b13b9cbcf3fa6e
krunoo53k <petrik.kpetrik@gmail.com>
2022-09-12 00:06:37 +0200

    Added kviz CRUD and ratings

--- server/server/routes/router.js
+++ server/server/routes/router.js
@@ -1,7 +1,9 @@
+const { Router } = require("express");
 const express = require("express");
 const passport = require("passport");
 const route = express.Router();
 const authController = require("../controllers/authController.js");
+const kvizController = require("../controllers/kvizController.js");
 
 route.get("/", (req, res) => {
   res.send("Crud App!");
@@ -11,5 +13,12 @@ route.post("/login", passport.authenticate("local"), authController.login);
 route.get("/user", authController.user);
 route.get("/logout", authController.logout);
 route.post("/register", authController.register);
+
+route.post("/kviz", kvizController.create);
+route.post("/kviz/:id/rating", kvizController.addRating);
+route.get("/kviz", kvizController.find);
+route.get("/kviz/:id", kvizController.findById);
+route.put("/kviz/:id", kvizController.update);
+route.delete("/kviz/:id", kvizController.delete);
 
 module.exports = route;
