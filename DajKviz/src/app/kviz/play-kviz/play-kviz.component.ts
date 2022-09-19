70aecd82163f2a76ff714b1bc834f9aed511973c
Bernarda Crnogorac <beacrnogorac@gmail.com>
2022-09-19 15:44:37 +0200

    Adding play kviz component

---
+++ DajKviz/src/app/kviz/play-kviz/play-kviz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-play-kviz',
+  templateUrl: './play-kviz.component.html',
+  styleUrls: ['./play-kviz.component.css']
+})
+export class PlayKvizComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
